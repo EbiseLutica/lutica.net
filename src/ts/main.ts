bcd34a4f951bcbb9b42ccf2ca908e29c359abcc2
Xeltica <xeltica@gmail.com>
2022-05-17 11:11:49 +0900

    Discordリンクを追加

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,11 @@
-const centerize = (profileCard: HTMLElement) => {
-  if (profileCard) {
-    const top = window.innerHeight / 2 - profileCard.getBoundingClientRect().height / 2;
-    profileCard.style.marginTop = top + "px";
-  }
-};
+const handleProfileCard = () => {
+  const centerize = (profileCard: HTMLElement) => {
+    if (profileCard) {
+      const top = window.innerHeight / 2 - profileCard.getBoundingClientRect().height / 2;
+      profileCard.style.marginTop = top + "px";
+    }
+  };
 
-
-window.addEventListener("load", () => {
   const profileCard = document.getElementById("profileCard");
   if (!profileCard) throw new Error("いや～～～、profileCardが見つからないのはバグで…");
   centerize(profileCard);
@@ -15,4 +14,24 @@ window.addEventListener("load", () => {
     centerize(profileCard);
   });
   observer.observe(document.body);
+};
+
+const handleDiscordButton = () => {
+  const discordButton = document.getElementById("discordButton");
+  if (!discordButton) throw new Error("いや～～～、profileCardが見つからないのはバグで…");
+  discordButton.addEventListener("click", () => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(discordButton.dataset.discordTag ?? "").then(() => {
+        alert("Discordタグをクリップボードにコピーしました！");
+      });
+    } else {
+      alert("Discordタグをクリップボードにコピーしようとしましたが、お使いの環境ではできません。");
+    }
+  });
+};
+
+
+window.addEventListener("load", () => {
+  handleProfileCard();
+  handleDiscordButton();
 });

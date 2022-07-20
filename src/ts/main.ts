e300734a947caee865286c1c638e6adb467f08d6
Xeltica <xeltica@gmail.com>
2022-07-21 03:07:59 +0900

    ローディング

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,4 +2,5 @@ window.addEventListener("load", () => {
   document.querySelectorAll("._cloak").forEach(el => {
     el.classList.add("_cloak--active");
   });
+  document.getElementById("shrimp-loader")?.remove();
 });

535450396b408b0ce7665562c25931884eb2b8a9
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2022-12-16 22:36:13 +0900

    コミットにギャグを込みっとｗ

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -29,23 +29,7 @@ const handleDiscordButton = () => {
   });
 };
 
-const handleTimelineParallax = () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      }
-    });
-  }, {
-    threshold: 0.5
-  });
-  document.querySelectorAll(".__timelineParallax").forEach(it => {
-    observer.observe(it);
-  });
-};
-
 window.addEventListener("load", () => {
   handleProfileCard();
   handleDiscordButton();
-  handleTimelineParallax();
 });
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,4 +3,20 @@ window.addEventListener("load", () => {
     el.classList.add("_cloak--active");
   });
   document.getElementById("shrimp-loader")?.remove();
+  handleTimelineParallax();
 });
+
+const handleTimelineParallax = () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  }, {
+    threshold: 0.5
+  });
+  document.querySelectorAll(".__timelineParallax").forEach(it => {
+    observer.observe(it);
+  });
+};

32953e666b8578008b6c45f1441a85bb4d659ab6
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-07-09 00:52:55 +0900

    ウェブサイト更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()]
 });

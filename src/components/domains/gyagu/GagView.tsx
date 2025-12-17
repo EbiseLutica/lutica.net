cd352ab65a5b76e4da4ad64b759a9e16c3492d76
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2025-12-18 04:17:10 +0900

    gyaguページをちょっと編集

--- src/components/domains/gyagu/GagView.tsx
+++ src/components/domains/gyagu/GagView.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
+const Gag: React.FC<{text: string | [string, string]}> = ({text}) => {
+	if (typeof text === 'string') {
+		return text;
+	}
+
+	return (
+		<details>
+			<summary>{text[0]}</summary>
+			{text[1]}
+		</details>
+	);
+};
+
 export const GagView: React.FC = () => {
 	const [gagList, setGagList] = useState<(string | [string, string])[]>([]);
 
@@ -16,13 +29,8 @@ export const GagView: React.FC = () => {
 			<p>現在のギャグ数：{gagList.length}個</p>
 			<ul>
 				{gagList.map(g => (
-					<li>
-						{typeof g === 'string' ? g : (
-							<details>
-								<summary>{g[0]}</summary>
-								{g[1]}
-							</details>
-						)}
+					<li key={typeof g === 'string' ? g : g[0] + g[1]}>
+						<Gag text={g} />
 					</li>
 				))}
 			</ul>
